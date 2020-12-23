3f82593ab25db0056ccbce22920ba62450fc94c0
emmorris93 <maudi.morris@gmail.com>
2020-12-22 16:35:54 -0800

    Add project files.

---
+++ MusicVideo.WebSite/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    var goUp = false;
+    function frame() {
+
+        if (pos == 0) goUp = false;
+        else if (pos == 200) goUp = true;
+
+        if (goUp) {
+            pos--;
+            elem.style.top = pos + "px";
+            elem.style.bottom = pos + "px";
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.bottom = pos + "px";
+        }
+    }
+}
+
+// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("ready!");
+    myMove();
+});
